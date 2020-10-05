8c133fc9a98e03f4269f4e8083b51d1dcf6021e8
mtsch <matijacufar@gmail.com>
2020-10-05 08:34:41 +0200

    fix compatiblity with older Julia

--- src/app.jl
+++ src/app.jl
@@ -30,7 +30,9 @@ function app(
     slider_values=1000,
 )
     outer_padding = 20
-    scene, layout = layoutscene(outer_padding; resolution, backgroundcolor)
+    scene, layout = layoutscene(
+        outer_padding; resolution=resolution, backgroundcolor=backgroundcolor
+    )
 
     flt_axis = layout[1:2, 1:2] = LScene(scene, title="Filtration")
     dgm_axis = layout[1, 3] = LAxis(scene, title="Diagram")
@@ -52,38 +54,17 @@ function app(
     title_text = @lift string("t = ", rpad($t, 6)[1:6])
     title = layout[0, :] = LText(scene, title_text, textsize=30)
 
-    plot!(flt_axis, filtration, points; time=t, palette)
+    plot!(flt_axis, filtration, points; time=t, palette=palette)
     if cameracontrols(flt_axis.scene) isa Camera3D
         cameracontrols(flt_axis.scene).rotationspeed[] = 0.01f0
     end
 
-    plot_diagram!(dgm_axis, diagram; time=t, palette)
-    plot_barcode!(bcd_axis, diagram; time=t, palette)
+    plot_diagram!(dgm_axis, diagram; time=t, palette=palette)
+    plot_barcode!(bcd_axis, diagram; time=t, palette=palette)
 
     linkxaxes!(dgm_axis, bcd_axis)
     tightlimits!(dgm_axis)
     tightlimits!(bcd_axis)
 
     return scene
-end
-
-function movie(points, filtration=Rips(points);
-               palette=DEFAULT_PALETTE,
-               resolution=(1600, 900),
-               backgroundcolor=:white,
-               t_start=minimum(Ripserer.births.(filtration)),
-               t_end=threshold(filtration),
-               n_steps=1000,
-               fps=30,
-               filename="out.mkv",
-               ripserer_kwargs...)
-    time = Observable(t_start)
-    scene = app(
-        points, filtration; palette, resolution, backgroundcolor, time, ripserer_kwargs...
-    )
-    r = range(t_start, t_end, length=n_steps)
-    record(scene, filename, r; framerate=fps) do t
-        println("t = $t / $t_end")
-        time[] = t
-    end
 end
